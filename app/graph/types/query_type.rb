a51864a594d1c114280fdebea65b6c7853e58daf
Americo Savinon <americos@gmail.com>
2016-01-20 17:35:46 -0500

    Commenting Star Wars Fields so that they dont appear on the query help in GraphiQL

--- app/graph/types/query_type.rb
+++ app/graph/types/query_type.rb
@@ -8,20 +8,20 @@ QueryType = GraphQL::ObjectType.define do
   description "The query root for this schema"
 
   # You can define fields on the fly:
-  field :hero do
-    type -> { !CharacterInterface }
-    description "The hero of the saga"
-
-    argument :episode, EpisodeEnum, "If provided, return the hero of that episode"
-
-    resolve -> (obj, args, ctx) do
-      args["episode"] == 5 ? Human.find(1000) : Droid.find(2001)
-    end
-  end
-
-  field :human, HumanType, field: FetchField.new(type: HumanType, model: Human)
-  field :droid, DroidType, field: FetchField.new(type: DroidType, model: Droid)
-  field :node, field: NodeIdentification.field
+  # field :hero do
+  #   type -> { !CharacterInterface }
+  #   description "The hero of the saga"
+  #
+  #   argument :episode, EpisodeEnum, "If provided, return the hero of that episode"
+  #
+  #   resolve -> (obj, args, ctx) do
+  #     args["episode"] == 5 ? Human.find(1000) : Droid.find(2001)
+  #   end
+  # end
+  #
+  # field :human, HumanType, field: FetchField.new(type: HumanType, model: Human)
+  # field :droid, DroidType, field: FetchField.new(type: DroidType, model: Droid)
+  # field :node, field: NodeIdentification.field
 
   field :reminder, ReminderType, field: FetchField.new(type: ReminderType, model: Reminder)
   field :all_reminders do
