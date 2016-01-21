f887e774939ca30b3c353f38b1c7cf00bbb3c23f
Americo Savinon <americos@gmail.com>
2016-01-21 16:31:10 -0500

    Using fragments for queries on specs

--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -9,19 +9,32 @@ describe 'ReminderType' do
     pp res if res['errors']
     res
   }
+  let(:reminder_info_fragment){
+    %|
+    fragment reminder_info on Reminder{
+      name, tool, due_date
+    }
+    |
+  }
 
   describe "Getting all reminders" do
-    let(:number_reminders){ 5 }
+    let(:total_reminders){ 5 }
     let(:query){
-      %|{ all_reminders: all_reminders{ name, tool, due_date} }|
+      %|
+      { all_reminders: all_reminders{
+          ... reminder_info
+        }
+      }
+      #{reminder_info_fragment}
+      |
     }
 
     before(:each) do
-      (1..number_reminders).each{|_| create(:reminder)}
+      (1..total_reminders).each{|_| create(:reminder)}
     end
 
     it "returns the correct amount of reminders" do
-      expect(result['data']['all_reminders'].count).to eq number_reminders
+      expect(result['data']['all_reminders'].count).to eq total_reminders
     end
   end
 end
